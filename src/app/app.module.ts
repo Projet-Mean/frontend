22afdbf8810aa53d2e484ce27b6717456deb91b1
Andrianjafy1 <sbaovola@gmail.com>
2023-01-28 12:06:17 +0300

    resolution conflit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,16 +15,12 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
   ],
   imports: [
     BrowserModule,
-    BrowserModule,
     AppRoutingModule,
-<<<<<<< HEAD
     FormsModule,
     ReactiveFormsModule,
     RouterModule
-=======
-    RouterModule,
-    FormsModule
->>>>>>> a07ead9700db6dbd0e94e0b27de94f829ccc7166
+
+
 
   ],
   schemas:[CUSTOM_ELEMENTS_SCHEMA],
